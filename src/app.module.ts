291a9a3243a2c2a4e01fdac3cefaf1e6a793682f
thefomin <raveyt@yandex.ru>
2025-10-21 08:00:03 +0700

    feat: modules services added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { BotModule } from './bot/bot.module';
+import { PaymentModule } from './api/payment/payment.module';
+import { TicketModule } from './api/ticket/ticket.module';
+import { PrismaModule } from './infra/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
+    BotModule,
+    PaymentModule,
+    TicketModule,
   ],
 })
 export class AppModule {}
